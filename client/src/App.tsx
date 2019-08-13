fffb9a126b662b7107e679bf3fec9eb026989500
Clark Kenneth Racadio <kennethracadio@hotmail.com>
2019-08-13 12:04:05 -0400

    Initial client commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  const message: string = "hello world";
+  return (
+    <div>
+      {message}
+    </div>
+  );
+}
+
+export default App;
